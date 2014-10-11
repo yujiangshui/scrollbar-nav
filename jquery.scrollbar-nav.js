e0b023b4bf0c520639796b99e7283e531098fa55
Jiangshui Yu <jiangshui.yjs@alibaba-inc.com>
2014-10-11 11:03:05 +0800

    fix

--- jquery.scrollbar-nav.js
+++ jquery.scrollbar-nav.js
@@ -10,27 +10,27 @@
 			var settings = $.extend( {}, this.defaults, options );
 			return this.each( function() {
 
-				var $this = $( this );
+					var $this = $( this );
 
-				//DOM 结构创建
-				var tempDOM = $('<ul class="scroll-nav"></ul>');
-				var bodyHeight = $('body').height() + 40;
-				$this.find('h1,h2,h3,h4,h5,h6')
-					.each(function(index, elem) {
-						var itemOffsetTop = $(elem).offset().top - parseInt(settings.topOffset);
-						var windowHeight = $(window).height();
-						//计算 item 在边栏的位置
-						var scrollbarOffsetTop = (itemOffsetTop / bodyHeight) * windowHeight;
-						var navItem = '<li style="top:'+scrollbarOffsetTop+'px" class="scroll-nav-'+elem.tagName.toLowerCase()+'" data-offset-top="'+itemOffsetTop+'px">'+$(elem).text()+'</li>';
-						tempDOM.append(navItem);
-				});
-				$('body').append(tempDOM);
+					//DOM 结构创建
+					var tempDOM = $('<ul class="scroll-nav"></ul>');
+					var bodyHeight = $('body').height() + 40;
+					$this.find('h1,h2,h3,h4,h5,h6')
+						.each(function(index, elem) {
+							var itemOffsetTop = $(elem).offset().top - parseInt(settings.topOffset);
+							var windowHeight = $(window).height();
+							//计算 item 在边栏的位置
+							var scrollbarOffsetTop = (itemOffsetTop / bodyHeight) * windowHeight;
+							var navItem = '<li style="top:'+scrollbarOffsetTop+'px" class="scroll-nav-'+elem.tagName.toLowerCase()+'" data-offset-top="'+itemOffsetTop+'px">'+$(elem).text()+'</li>';
+							tempDOM.append(navItem);
+					});
+					$('body').append(tempDOM);
 
-				//单击滚动功能实现
-				$('body').on('click','.scroll-nav li',function() {
-					var offsetTop = $(this).data('offset-top');
-					$('body').animate({scrollTop: offsetTop},200);
-				});
+					//单击滚动功能实现
+					$('body').on('click','.scroll-nav li',function() {
+						var offsetTop = $(this).data('offset-top');
+						$('body').animate({scrollTop: offsetTop},200);
+					});
 
 			});
 		}
